1f147241bb7b7461b008e997bb0e7107fbe49ae0
vincentparra <vincent.jovan.resurreccion.parra@gmail.com>
2025-06-05 09:06:55 +0800

    added CI (#8)

--- src/main/java/com/rocs/sample/plugin/SamplePluginApplication.java
+++ src/main/java/com/rocs/sample/plugin/SamplePluginApplication.java
@@ -1,7 +1,10 @@
 package com.rocs.sample.plugin;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class SamplePluginApplication {
     public static void main(String[] args) {
-        System.out.println("hello this is my first plugin");
+        System.out.println("hello this is my first plugin run in CI");
     }
 }
