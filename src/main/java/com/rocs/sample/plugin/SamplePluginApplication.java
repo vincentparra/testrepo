b32187d781286370aa56ddf1c5b50074ae616100
vincentparra <vincent.jovan.resurreccion.parra@gmail.com>
2025-06-05 08:36:44 +0800

    initial commit

---
+++ src/main/java/com/rocs/sample/plugin/SamplePluginApplication.java
@@ -0,0 +1,7 @@
+package com.rocs.sample.plugin;
+
+public class SamplePluginApplication {
+    public static void main(String[] args) {
+        System.out.println("hello this is my first plugin");
+    }
+}
