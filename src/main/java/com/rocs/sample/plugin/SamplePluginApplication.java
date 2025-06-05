aea0750721d7a2b10f742633d8224b95e7a7d850
vincentparra <vincent.jovan.resurreccion.parra@gmail.com>
2025-06-05 09:06:02 +0800

    added CI

--- src/main/java/com/rocs/sample/plugin/SamplePluginApplication.java
+++ src/main/java/com/rocs/sample/plugin/SamplePluginApplication.java
@@ -5,14 +5,6 @@ import java.io.IOException;
 
 public class SamplePluginApplication {
     public static void main(String[] args) {
-        String name = System.getenv("INPUT_NAME");
-        String message = "Hello, " + name;
-
-        try (FileWriter writer = new FileWriter(System.getenv("GITHUB_OUTPUT"), true)) {
-            writer.write("message=" + message + "\n");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Java Action executed with name: " + name);
+        System.out.println("hello this is my first plugin run in CI");
     }
 }
